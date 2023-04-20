056fa9b38d215b65710d782e1134f1bb43e0dcf2
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-04-20 11:56:31 +0800

    Tested letter a

--- decrypt.py
+++ decrypt.py
@@ -2,10 +2,17 @@
 encrypt_text = input("Enter a string to decrypt: ")
 decrypt_text = ""
 # loop through entire characters
+for char in encrypt_text:
 #   if * change to a
+    if char == "*":
+        decrypt_text += "a"
 #   if & change to e
 #   if # change to i
 #   if + change to o
 #   if ! change to u
+# if not vowel
+    else:
+        decrypt_text += char
 # print the output
+print(decrypt_text)
 # Create design
