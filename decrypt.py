317e75beb6d8d206a65345cc508a8d0dfcaaa0d2
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-04-20 11:47:28 +0800

    setting up input and output

---
+++ decrypt.py
@@ -0,0 +1,11 @@
+# ask for string input to decrypt, save it
+encrypt_text = input("Enter a string to decrypt: ")
+decrypt_text = ""
+# loop through entire characters
+#   if * change to a
+#   if & change to e
+#   if # change to i
+#   if + change to o
+#   if ! change to u
+# print the output
+# Create design
